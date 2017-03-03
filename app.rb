58190bd2b5875e1a0b462aa29ee7dbeaef6ac400
rost <rostislav.korin@gmail.com>
2017-03-03 13:29:31 +0200

    creating products

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ require 'json'
     erb :home
   end
 
-  post '/request' do
+  post '/get_json' do
     @shop_json = RestClient.get("https://demo.shop2market.com/api/v1/shops.json", {"X-Api-Key" => params[:api_key]})
     @shop = JSON.parse(@shop_json)
 
@@ -20,6 +20,26 @@ require 'json'
      publishers_json = RestClient.get("https://demo.shop2market.com/api/v1/shops/#{shop["id"]}/publishers.json", {"X-Api-Key" => params[:api_key]})
      @publishers[shop["id"]] = JSON.parse(publishers_json)
     end
-
      erb :output
   end
+
+  post '/post_json' do
+      headers = {"X-Api-Key" => params[:api_key], content_type: :json, accept: :json}
+      @products_json = RestClient.post("https://demo.shop2market.com/api/v1/shops/#{params[:shop_id]}/shop_products/batch",
+                                          [{"shop_code": "#{params[:hop_code]}",
+                                            "variant_id": "#{params[:variant_id]}",
+                                            "deeplink": "#{params[:deeplink]}",
+                                            "product_name": "#{params[:name]}",
+                                            "selling_price": params[:priceinctax],
+                                            "selling_price_ex": params[:pricenotax],
+                                            "picture_link": "#{params[:picturelink]}"}].to_json, headers) { |response, request, result|
+                                                                                                            case response.code
+                                                                                                            when 200, 201, 202, 203, 204
+                                                                                                              erb :created_products
+                                                                                                            else
+                                                                                                              erb :wasnt_created
+                                                                                                            end
+                                                                                                          }
+
+
+    end
